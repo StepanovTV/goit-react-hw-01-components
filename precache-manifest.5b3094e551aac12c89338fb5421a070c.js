d46a9691768221392f3325fa65d4d876b455889f
taras <stvstudio.com.ua@gmail.com>
2019-09-30 10:59:07 +0300

    Updates

---
+++ precache-manifest.5b3094e551aac12c89338fb5421a070c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20482205292855b2b68b1c45f89618ea",
+    "url": "/goit-react-hw-01-components/./index.html"
+  },
+  {
+    "revision": "cc3382c605733f95270a",
+    "url": "/goit-react-hw-01-components/./static/css/main.17b89671.chunk.css"
+  },
+  {
+    "revision": "3c9140ef63bffab41fb5",
+    "url": "/goit-react-hw-01-components/./static/js/2.5cba945c.chunk.js"
+  },
+  {
+    "revision": "cc3382c605733f95270a",
+    "url": "/goit-react-hw-01-components/./static/js/main.e9e2cf7f.chunk.js"
+  },
+  {
+    "revision": "4748e4de63b13fcdc14d",
+    "url": "/goit-react-hw-01-components/./static/js/runtime-main.bb7b8e62.js"
+  }
+]);
